b14205ffde54e5fa8d7eca7f88650be1d20e762e
chinh4thepro <chinh4thepro@gmail.com>
2025-05-12 22:26:23 -0400

    11-22-2022

--- 02-21-2023/config/wpg/wp_init.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-wpg -rs 'minimal_mountains.png' 'minimal_mountains.png'
--- 07-29-2023/cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#adc7e0", "#0A0C1B" },
-	[SchemeSel] = { "#adc7e0", "#8B6372" },
-	[SchemeOut] = { "#adc7e0", "#618AAB" },
-};
